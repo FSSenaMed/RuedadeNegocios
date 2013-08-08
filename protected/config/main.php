43840ee8c98aaf89e4810ef3ec127c29400a34ed
aquirozr <aquirozr@gmail.com>
2013-08-08 11:11:14 -0500

    2013-08-08 con users
    con usuarios

--- protected/config/main.php
+++ protected/config/main.php
@@ -22,6 +22,8 @@ return array(
 	'import'=>array(
 		'application.models.*',
 		'application.components.*',
+                'application.modules.user.models.*',
+                'application.modules.user.components.*',
 	),
     
         
@@ -37,6 +39,37 @@ return array(
 		
                         'generatorPaths'=>array('bootstrap.gii',),
                     ),
+                    'user'=>array(
+                    # encrypting method (php hash function)
+                    'hash' => 'md5',
+
+                    # send activation email
+                    'sendActivationMail' => true,
+
+                    # allow access for non-activated users
+                    'loginNotActiv' => false,
+
+                    # activate user on registration (only sendActivationMail = false)
+                    'activeAfterRegister' => false,
+
+                    # automatically login from registration
+                    'autoLogin' => true,
+
+                    # registration path
+                    'registrationUrl' => array('/user/registration'),
+
+                    # recovery password path
+                    'recoveryUrl' => array('/user/recovery'),
+
+                    # login form path
+                    'loginUrl' => array('/user/login'),
+
+                    # page after login
+                    'returnUrl' => array('/user/profile'),
+
+                    # page after logout
+                    'returnLogoutUrl' => array('/user/login'),
+                ),
 		
 	),
 
@@ -71,14 +104,23 @@ return array(
 			'connectionString' => 'mysql:host=localhost;dbname=db_ruedadenegocios',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => 'userfabdev',
+			'password' => 'root',
 			'charset' => 'utf8',
+                        'tablePrefix' => 'tbl_',
 		),
+            
+                'user'=>array(
+                    // enable cookie-based authentication
+                    'class' => 'WebUser',
+                    'allowAutoLogin'=>true,
+                    'loginUrl' => array('/user/login'),
+                ),
 		
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
 		),
+            
 		'log'=>array(
 			'class'=>'CLogRouter',
 			'routes'=>array(
